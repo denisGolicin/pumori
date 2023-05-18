c8f5d2d83397eac85d31a409dd3e4566f8e32f32
denisGolicin <den.golicin65@gmail.com>
2023-05-18 17:03:24 +0200

    commit

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 
 let buttons = [] = document.querySelectorAll('.button-model');
 let model = document.querySelector('.model-style');
+let buttonActive = buttons[0];
+buttonActive.style.boxShadow = '0 0 5px #fff';
 
 for(let i = 0; i < buttons.length; i++){
     buttons[i].addEventListener('click', function(){
         model.src = `src/models/Detal_${i}.glb`;
+        buttonActive.style.boxShadow = '0 0 5px black';
+        this.style.boxShadow = '0 0 5px #fff';
+        buttonActive = buttons[i];
     })
 }
